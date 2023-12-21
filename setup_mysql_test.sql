704cbf2a7a702519fb86cd2f854aa6c0503ccbf9
MarwaAbuEssa <meromeroo@gmail.com>
2023-12-21 03:53:09 +0200

    fixing in models

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,16 +4,14 @@
 --   Grants all privileges for hbnb_test on hbnb_test_db.
 --   Grants SELECT privilege for hbnb_test on performance_schema.
 
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER
     IF NOT EXISTS 'hbnb_test'@'localhost'
     IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES
-   ON `hbnb_test_db`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
+   ON *.* TO 'hbnb_test'@'localhost'
+   WITH GRANT OPTION;
 GRANT SELECT
    ON `performance_schema`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
+   TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
