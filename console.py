f6927c9a910879375c60b3465185b91a9def9848
MarwaAbuEssa <meromeroo@gmail.com>
2023-12-21 02:05:48 +0200

    fix in do_create

--- console.py
+++ console.py
@@ -114,33 +114,33 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, line):
-    """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
-    Create a new class instance with given keys/values and print its id.
-    """
-    try:
-    if not line:
-    raise SyntaxError()
-    my_list = line.split(" ")
-
-    kwargs = {}
-    for i in range(1, len(my_list)):
-    key, value = tuple(my_list[i].split("="))
-    if value[0] == '"':
-        value = value.strip('"').replace("_", " ")
-    else:
+        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with given keys/values and print its id.
+        """
         try:
-            value = eval(value)
-        except (SyntaxError, NameError):
-            continue
-    kwargs[key] = value
+            if not line:
+                raise SyntaxError()
+            my_list = line.split(" ")
 
-    if kwargs == {}:
-    obj = eval(my_list[0])()
-    else:
-    obj = eval(my_list[0])(**kwargs)
-    storage.new(obj)
-    print(obj.id)
-    obj.save()
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
+                    value = value.strip('"').replace("_", " ")
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kwargs[key] = value
+
+            if kwargs == {}:
+                obj = eval(my_list[0])()
+            else:
+                obj = eval(my_list[0])(**kwargs)
+                storage.new(obj)
+            print(obj.id)
+            obj.save()
 
     def help_create(self):
         """ Help information for the create method """
